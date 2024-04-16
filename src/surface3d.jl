e177893b0a9dc46b6f7db523e0253316d829691f
Sanjay Lall <sanjaylall@google.com>
2024-04-16 11:25:30 -0700

    Remove dead code

--- src/surface3d.jl
+++ src/surface3d.jl
@@ -13,12 +13,9 @@ using ..AxisDrawables3D: AxisDrawable3
 
 export SurfaceOptions, parse_raytrace_options, Raytrace, surf, drawraytrace
 
-#
 # A Surface is a subtype of Shape, something that can be raytraced.
-#
 
 const pk = PlotKit
-
 
 Base.@kwdef mutable struct SurfaceOptions
     xmin = -1
@@ -31,8 +28,6 @@ Base.@kwdef mutable struct SurfaceOptions
     samplegradient = true
     texture1 = Grid(:red)
     texture2 = Grid(:green)
-    #rto = RaytraceOptions()
-    # below was in RaytraceOptions
     lighting = Lighting()
     refine = true
     shadows = false
@@ -41,14 +36,10 @@ Base.@kwdef mutable struct SurfaceOptions
     renderheight = missing
 end
 
-
-# why is Raytrace separate from RaytraceOptions?
-# We should just unify these.
 Base.@kwdef mutable struct Raytrace
     box
     shapes
     axis3 = nothing
-    # below was in RaytraceOptions
     lighting = Lighting()
     refine = true
     shadows = false
@@ -59,51 +50,20 @@ end
 
 
 function PlotKit.draw(ad::AxisDrawable3, rt::Raytrace)
-    #drawraytrace(ctx, axis3, shapes, box, rto::RaytraceOptions)
     renderwidth = ifnotmissing(rt.renderwidth, rt.axis3.width)
     renderheight = ifnotmissing(rt.renderheight, rt.axis3.height)
     camera = Camera(rt.axis3.axismap3, rt.axis3.width,
                     rt.axis3.height, renderwidth, renderheight)
-
     shapes_in_cube_coords = [transform(s, rt.axis3) for s in rt.shapes]
     for s in shapes_in_cube_coords
         addlimits(s, rt.axis3.axismap3.cube)
     end
-     
-
     X = raytrace_main(camera, rt.lighting,
                       shapes_in_cube_coords,
                       rt.refine, rt.shadows)
-    
     image = cairoimagefrommatrix(X)
     drawimage(ad.ctx, camera, image)
 end
-
-
-#function raytrace(shapes, box, rto::RaytraceOptions)
-#    axis3 = Axis3(box, rto.axisoptions3)
-#    d = Drawable(axis3.width, axis3.height)
-#    rt = Raytrace(box, shapes, axis3, rto)
-#    return rt
-#end
-# 
-# 
-# function extra_raytrace_options!(rto; kw...)
-#     setoptions!(rto.axisoptions3, "axisoptions3_", kw...)
-#     setoptions!(rto.axisoptions3.tickbox, "tickbox_", kw...)
-#     setoptions!(rto.axisoptions3.axisbox, "axisbox_", kw...)
-#     setoptions!(rto.axisoptions3.ticks, "ticks_", kw...)
-#     setoptions!(rto.axisoptions3.axisstyle3, "axisstyle3_", kw...)
-# end
-#     
-#     
-# function parse_raytrace_options(; kw...)
-#     rto = RaytraceOptions()
-#     setoptions!(rto, "", kw...)
-#     extra_raytrace_options!(rto; kw...)
-#     return rto
-# end
-# 
 
 function Raytrace(shapes, box; kw...)
     rt = Raytrace(; shapes, box, allowed_kws(Raytrace, kw)...)
@@ -113,33 +73,19 @@ function Raytrace(shapes, box; kw...)
     setoptions!(rt.axisoptions3.ticks, "ticks_", kw...)
     setoptions!(rt.axisoptions3.axisstyle3, "axisstyle3_", kw...)
     rt.axis3 = Axis3(box, rt.axisoptions3)
-#    rt = raytrace(shapes, box, rto)
-    #d = Drawable(axis3.width, axis3.height)
-    #rt = Raytrace(box, shapes, axis3, rto)
-    
     return rt
 end
-
-
-#function PlotKit.draw(rt::Raytrace)
-#    d = Drawable(rt.axis3.width, rt.axis3.height)
-#    drawaxis3(d.ctx, rt.axis3)
-#    drawraytrace(d.ctx, rt.axis3, rt.shapes, rt.box, rt.rto)
-#    return d
-#end
 
 function PlotKit.draw(rt::Raytrace)
     ad = AxisDrawable3(rt.axis3)
     drawaxis3(ad)
-    #drawraytrace(ad.ctx, rt.axis3, rt.shapes, rt.box, rt.rto)
     draw(ad, rt)
     return ad
 end
 
-    
+   
 ##############################################################################
 # surface
-
 
 function getlimitsfromfn(box2, zfun::Function)
     x = range(box2.xmin, box2.xmax, length = 100)
